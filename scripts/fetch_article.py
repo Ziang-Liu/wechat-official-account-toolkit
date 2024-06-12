5b3c9ca196b7f9f75937feefb46d6e86e15822a3
dariNirvana <takina0721@gmail.com>
2024-06-12 15:11:00 +0800

    :sparkles: Rearrange code

--- scripts/fetch_article.py
+++ scripts/fetch_article.py
@@ -20,30 +20,32 @@ class WechatOfficialAccountFetcher:
         self._base_url = "https://mp.weixin.qq.com/cgi-bin/appmsgpublish"
         self._headers = {'Cookie': cookie, 'User-Agent': UserAgent().random}
         self._params = parse_qs(urlparse(url).query)
-        self._page = start_page
-        self._end_page = end_page
+        self._active_page = start_page
+        self._target_page = end_page
+        self._fetched_pages = 0
 
         with open("result.csv", "w", encoding = "utf-8") as f:
             f.write("title,cover,link,author_name\n")
 
     async def get_published_articles(self) -> None:
         async with AsyncClient(headers = self._headers, verify = False, timeout = 10) as client:
-            while self._page != self._end_page:
-                self._params['begin'] = [str(self._page * 5)]
-                result = (await client.get(params = self._params, url = self._base_url)).json()
-                if result['base_resp']['ret'] == 200013:
-                    logger.warning("触发反爬限制，等半个小时再继续")
+            while self._active_page != self._target_page:
+                self._params['begin'] = [str(self._active_page * 5)]
+                resp = (await client.get(params = self._params, url = self._base_url)).json()
+                if resp['base_resp']['ret'] == 200013:
+                    logger.warning("触发反爬限制，暂停半小时")
                     await asyncio.sleep(1800)
                     await self.get_published_articles()
+                elif "publish_page" not in resp:
+                    logger.info("获取完毕")
+                    return
 
-                if "publish_page" in result:
-                    publish_list = json.loads(result["publish_page"])["publish_list"]
-                    for pl in publish_list:
-                        appmsgex = json.loads(pl["publish_info"])["appmsgex"]
-                        for ame in appmsgex:
-                            with open("result.csv", 'a', encoding = 'utf-8') as f:
-                                f.write(f"{ame['title']},{ame['cover']},{ame['link']},{ame['author_name']}\n")
+                for i in json.loads(resp["publish_page"])["publish_list"]:
+                    with open("result.csv", 'a', encoding = 'utf-8') as f:
+                        for j in json.loads(i["publish_info"])["appmsgex"]:
+                            f.write(f"{j['title']},{j['cover']},{j['link']},{j['author_name']}\n")
+                            self._fetched_pages += 1
 
-                    self._page += 1
-                    logger.info(f"目前爬取的页: {self._page}, 爬取的文章数量: {self._page * 5}")
+                    self._active_page += 1
+                    logger.info(f"第 {self._active_page} 页, 总共 {self._fetched_pages} 篇文章")
                     await asyncio.sleep(randint(30, 60))
