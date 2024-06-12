e0fe578c9cae9e18e0dfbdb2cb5694015bf0459a
dariNirvana <takina0721@gmail.com>
2024-06-12 15:11:21 +0800

    :sparkles: Polish instructions

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ async def download_images_from_result():
                                "1代表True, 0代表False:")
     filter_image_list = input("是否过滤掉图片少于五张的公众号文章\n"
                               "1代表True, 0代表False:")
+    keywords = []
+    while filter_keyword := input("输入过滤文章关键字，留空停止输入:\n"):
+        keywords.append(filter_keyword)
+
     filter_gif = input("是否需要 gif\n"
                        "1代表True, 0代表False:")
     start_from = input("需要从result.csv的第几行开始呢(不输入保持默认0):")
@@ -25,6 +29,7 @@ async def download_images_from_result():
         thread = int(download_thread) if download_thread else 4,
         start_from = int(start_from) if start_from else 0,
         total_tasks = int(total_task) if total_task else -1,
+        keywords = keywords,
         filter_small_image = True if filter_small_image == "1" else False,
         filter_image_list = True if filter_image_list == "1" else False,
         filter_gif = True if filter_gif == "1" else False
@@ -32,14 +37,14 @@ async def download_images_from_result():
 
 
 async def get_official_account_publish():
-    print("\n查看一下 README.md 上的教程再开始使用哦")
+    print("\n***查看一下 README.md 上的教程再开始使用哦***\n\n")
     url = input("URL:")
     cookie = input("Cookie:")
 
-    print("\n输入起始页与终止页（如第0页代表最新的文章，数字越大文章发布时间时间越早\n"
+    print("\n输入起始页与终止页（如第0页代表最新的文章，数字越大文章发布时间时间越早\n\n"
           "获取的文章数量是 (终止页 - 起始页) * 5), 不输入保持默认")
-    start_page = input("起始页(默认0):")
-    end_page = input("终止页(默认-1):)")
+    start_page = input("起始页(默认从最新的文章获取):")
+    end_page = input("终止页(默认爬完所有历史文章):")
 
     os.makedirs(f"{os.getcwd()}/workdir", exist_ok = True)
     os.chdir(f"{os.getcwd()}/workdir")
